3e35c27bb5d5152626ce3ef0bd600849aa06ede0
Angelo Bennett <77983302+ahbenn86@users.noreply.github.com>
2022-08-22 14:43:14 -0400

    contact form | mobile response

--- script.js
+++ script.js
@@ -1,46 +1,54 @@
 const navbar = document.querySelector(".navbar");
 const navbarOffsetTop = navbar.offsetTop;
-const sections = document.querySelectorAll('section');
-const navbarLinks = document.querySelectorAll('.navbar-link');
+const sections = document.querySelectorAll("section");
+const navbarLinks = document.querySelectorAll(".navbar-link");
+const submitButton = document.getElementById("submit-button");
+const form = document.getElementById('form');
+const inputs = document.querySelectorAll('input');
+const message = document.querySelector('textarea');
 
-window.addEventListener("scroll", ()=>{
-    mainFn();
+window.addEventListener("scroll", () => {
+  mainFn();
 });
 
 const mainFn = () => {
-    if (window.pageYOffset >= navbarOffsetTop){
-        navbar.classList.add("sticky");
-    } else {
-        navbar.classList.remove("sticky");
-    }
+  if (window.pageYOffset >= navbarOffsetTop) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
 
-    sections.forEach((section, i) => {
-        if(window.pageYOffset >= section.offsetTop -10){
-            navbarLinks.forEach((navbarLink) => {
-                navbarLink.classList.remove("change");
-            });
-            navbarLinks[i].classList.add("change");
-        }
-    });
-}
+  sections.forEach((section, i) => {
+    if (window.pageYOffset >= section.offsetTop - 10) {
+      navbarLinks.forEach((navbarLink) => {
+        navbarLink.classList.remove("change");
+      });
+      navbarLinks[i].classList.add("change");
+    }
+  });
+};
 
 mainFn();
 
-    function sendMail(params) {
-        let tempParams = {
-            from_name: document.getElementById("name").value,
-            to_name: document.getElementById("toName").value,
-            message: document.getElementById("msg").value, 
-        };
+submitButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  const serviceID = "default_service";
+  const templateID = "template_x13eepf";
 
-      emailjs.sendForm('service_buul3eq', 'template_x13eepf', tempParams)
-        .then(function(res) {
-          console.log('SUCCESS!');
-        }, function (error) {
-          console.log('FAILED...', error);
-        });
+  emailjs.sendForm(serviceID, templateID, form).then(
+    () => {
+      submitButton.value = "Send Email";
+      alert("Message sent!");
+    },
+    (err) => {
+      submitButton.value = "Send Email";
+      alert(JSON.stringify(err));
     }
-  
+  );
+
+  inputs.forEach(input => input.value = '');
+  message.value = '';
+});
 
 // window.addEventListener('resize', () => {
 //     window.location.reload();
